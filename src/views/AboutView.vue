7ff31121a0acbaa1fbfda13c85f4ef2b615a3b97
Leo <leonardonaka75@gmail.com>
2024-05-03 08:49:55 -0300

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 class="mb-4 text-center">Meus dados</h1>
+    <div>
+      <h2>Nome:</h2>
+      <p>Leonardo Yuiti Nakamura</p>
+    </div>
+    <div>
+      <h2>RA:</h2>
+      <p>1975660</p>
+    </div>
+    <div>
+      <h2>Univesidade:</h2>
+      <p>UNIMAR</p>
+    </div>
+    <div>
+      <h2>Curso:</h2>
+      <p>Análise e desenvolvimento de sistemas</p>
+    </div>
+  </div>
+</template>
